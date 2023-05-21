0be2eb47175257e07215fad102d7a2129aa18265
Sushkov AS <aveslaizer@gmail.com>
2023-05-21 13:49:32 +0300

    Скопировал классы необходимые для ваполнения ДЗ

---
+++ expression.py
@@ -0,0 +1,34 @@
+class ExpressionConverter:
+    operation_priority = {
+        '(': 0,
+        '+': 1,
+        '-': 1,
+        '*': 2,
+        '/': 2,
+    }
+
+    @staticmethod
+    def to_postfix(expression: str) -> list:
+        pass
+
+
+class Expression:
+
+    def __init__(self, expression: str):
+        self.__infix_expression = expression
+        self.__postfix_expression = ExpressionConverter.to_postfix(expression)
+
+    @property
+    def infix_expression(self):
+        return self.__infix_expression
+
+    @property
+    def postfix_expression(self):
+        return self.__postfix_expression
+
+    def get_expression_value(self):
+        """
+        Возвращает значение выражения, записанного в постфиксной форме в поле __postfix_expression
+        :return:
+        """
+        pass
