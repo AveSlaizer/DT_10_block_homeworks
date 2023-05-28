6a702355194eb1603d821930ce314ea272a48d3d
Sushkov AS <aveslaizer@gmail.com>
2023-05-28 21:41:23 +0300

    Написал часть основной программы

--- data_structure/priority_queue.py
+++ data_structure/priority_queue.py
@@ -9,13 +9,25 @@ class VisitorPriorityQueue:
         return ' '.join([f"/{i}/\n" for i in self.__queue])
 
     def is_empty(self):
+        """ проверка очереди на пустоту """
         return len(self.__queue) == 0
 
-    def insert(self, item: Visitor):
+    def insert_with_priority(self, item: Visitor):
+        """ добавление элемента c приоритетом в очередь """
         if not isinstance(item, Visitor):
             raise TypeError(f"Недопустимый тип данных '{item.__class__.__name__}', ожидался 'User'")
         self.__queue.append(item)
         self.__queue.sort()
 
-    def delete(self):
+    def peek(self):
+        """ возврат самого большого по приоритету элемента """
+        return self.__queue[-1]
+
+    def pull_highest_priority_element(self):
+        """ удаление элемента c высшим приоритетом из очереди """
         return self.__queue.pop().__name
+
+    def show(self):
+        """ отображение всех элементов очереди на экран """
+        for visitor in self.__queue:
+            print(visitor)
