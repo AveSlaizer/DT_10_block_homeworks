7c8b337d27c959c543be9a7ab75aca7f00ac7f2f
Sushkov AS <aveslaizer@gmail.com>
2023-05-29 22:24:45 +0300

    Написал функции для кнопок интерфейса

--- data_structure/priority_queue.py
+++ data_structure/priority_queue.py
@@ -15,6 +15,19 @@ class Task:
     def priority(self, priority: int):
         self.__priority = priority
 
+    def __str__(self):
+        return f"Приоритет: {self.priority}. Описание:\n{self.description}"
+
+    def __lt__(self, other):
+        if not isinstance(other, Task):
+            raise TypeError(f"Недопустимый тип данных '{other.__class__.__name__}', ожидался 'Task'")
+        return self.priority < other.priority
+
+    def __gt__(self, other):
+        if not isinstance(other, Task):
+            raise TypeError(f"Недопустимый тип данных '{other.__class__.__name__}', ожидался 'Task'")
+        return self.priority > other.priority
+
 
 
 class PriorityQueue:
@@ -30,8 +43,8 @@ class PriorityQueue:
     def peek(self):
         if not self.is_empty():
             return self.__queue[-1]
-        return "Нет активных задач"
-        #raise IndexError("Нет активных задач")
+        raise IndexError("Отсутствует актуальная задача")
+
 
     def insert(self, task: Task):
         self.__queue.insert(0, task)
@@ -43,7 +56,7 @@ class PriorityQueue:
         raise IndexError("Отсутствует актуальная задача")
 
     def change_actual_task_priority(self, priority: int):
-        if not self.is_empty():
-            self.__queue[-1].priority = priority
-            self.__queue.sort()
-        raise IndexError("Отсутствует актуальная задача")
+        if self.is_empty():
+            raise IndexError("Отсутствует актуальная задача")
+        self.__queue[-1].priority = priority
+        self.__queue.sort()
--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -7,6 +7,7 @@ class MainWindow:
 
     def __init__(self):
         self.queue = PriorityQueue()
+        self.change_btn_state = "disabled"
 
         self.main_window = tkinter.Tk()
         self.main_window.title("To - Do List")
@@ -17,17 +18,19 @@ class MainWindow:
         self.main_window.rowconfigure(index=0, )
 
         self.actual_task = tkinter.StringVar()
-        self.actual_task.set(self.queue.peek())
+        self.actual_task.set("Нет активных задач")
 
         self.task_label = tkinter.Label(self.main_window, textvariable=self.actual_task, font=("Arial", 16),
-                                        width=40, height=10, borderwidth=3, relief="ridge")
+                                        width=40, height=10, borderwidth=3, relief="ridge", justify="left")
         self.task_label.grid(row=0, columnspan=2, sticky="wens", pady=4, padx=4)
 
+
         self.change_btn = tkinter.Button(self.main_window, text="Изменить", font=("Arial", 16),
-                                         command=self.__change_actual_task_priority, state="disabled")
+                                         command=self.__change_actual_task_priority, state=tkinter.DISABLED)
         self.change_btn.grid(row=1, column=0, sticky="we", pady=8, padx=4)
 
-        self.do_it_btn = tkinter.Button(self.main_window, text="Выполнить", font=("Arial", 16), state="disabled")
+        self.do_it_btn = tkinter.Button(self.main_window, text="Выполнить", font=("Arial", 16), state=tkinter.DISABLED,
+                                        command=self.__do_task)
         self.do_it_btn.grid(row=1, column=1, sticky="we", pady=8, padx=4)
 
         self.add_btn = tkinter.Button(self.main_window, text="Добавить", font=("Arial", 16), width=22,
@@ -48,13 +51,11 @@ class MainWindow:
         self.change_window = tkinter.Tk()
         self.change_window.title("Change priority")
 
-        self.new_priority = tkinter.StringVar()
-
         self.priority_change_spinbox = tkinter.Spinbox(self.change_window, from_=1, to=10, font=("Arial", 16))
-        #self.new_priority.set(self.priority_change_spinbox.get())
         self.priority_change_spinbox.grid(row=0, columnspan=2, pady=4, padx=4)
 
-        self.ok_btn = tkinter.Button(self.change_window, text="OK", font=("Arial", 16), width=10)
+        self.ok_btn = tkinter.Button(self.change_window, text="OK", font=("Arial", 16), width=10,
+                                     command=self.__change_priority)
         self.ok_btn.grid(row=1, column=0, pady=4, padx=4)
 
         self.cancel_change_btn = tkinter.Button(self.change_window, text="Отмена", font=("Arial", 16), width=10,
@@ -70,15 +71,15 @@ class MainWindow:
 
 
         self.description = tkinter.StringVar()
-        self.priority = tkinter.StringVar()
 
-        self.description_entry = tkinter.Entry(self.add_window, font=("Arial", 16))
+        self.description_entry = tkinter.Text(self.add_window, font=("Arial", 16), wrap="word", width=28, height=5)
         self.description_entry.grid(row=0, columnspan=2, pady=4, padx=4)
 
         self.priority_add_spinbox = tkinter.Spinbox(self.add_window, from_=1, to=10, font=("Arial", 16))
         self.priority_add_spinbox.grid(row=1, columnspan=2, pady=4, padx=4)
 
-        self.task_add_btn = tkinter.Button(self.add_window, text="Добавить", font=("Arial", 16), width=10)
+        self.task_add_btn = tkinter.Button(self.add_window, text="Добавить", font=("Arial", 16), width=10,
+                                           command=self.__add_new_task)
         self.task_add_btn.grid(row=2, column=0, pady=4, padx=4)
 
         self.cancel_add_btn = tkinter.Button(self.add_window, text="Отмена", font=("Arial", 16), width=10,
@@ -94,6 +95,53 @@ class MainWindow:
     def __destroy_change_window(self):
         self.main_window.state("normal")
         self.change_window.destroy()
+
+    def __add_new_task(self):
+        description = self.description_entry.get(1.0, tkinter.END)
+        priority = int(self.priority_add_spinbox.get())
+        if not description:
+            tkinter.messagebox.showwarning("Warning", "Задание должно иметь описание!")
+        else:
+            task = Task(description, priority)
+            self.queue.insert(task)
+            text = f"Приоритет: {self.queue.peek().priority}. " \
+                   f"Описание:\n{self.format_text(self.queue.peek().description)}"
+            self.actual_task.set(text)
+            self.change_btn["state"] = tkinter.NORMAL
+            self.do_it_btn["state"] = tkinter.NORMAL
+            self.__destroy_add_window()
+
+    def __change_priority(self):
+        new_priority = int(self.priority_change_spinbox.get())
+        self.queue.change_actual_task_priority(new_priority)
+        text = f"Приоритет: {self.queue.peek().priority}. " \
+               f"Описание:\n{self.format_text(self.queue.peek().description)}"
+        self.actual_task.set(text)
+        self.__destroy_change_window()
+
+    def __do_task(self):
+        done_task = self.queue.delete()
+        if self.queue.is_empty():
+            text = "Нет активных задач"
+            self.change_btn["state"] = tkinter.DISABLED
+            self.do_it_btn["state"] = tkinter.DISABLED
+        else:
+            text = f"Приоритет: {self.queue.peek().priority}. " \
+                   f"Описание:\n{self.format_text(self.queue.peek().description)}"
+        self.actual_task.set(text)
+        tkinter.messagebox.showinfo("Info", f"{done_task}\nЗадание выполнено")
+
+    @staticmethod
+    def format_text(text: str):
+        columns = 39
+        index = 1
+        while index < len(text):
+            if index % columns == 0:
+                text = text[:index] + "\n" + text[index:]
+            index += 1
+        return text
+
+
 
 
 
