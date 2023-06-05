43035ddeb16ebb3a8c3257e018dc6e7c69e91fe4
Sushkov AS <aveslaizer@gmail.com>
2023-06-05 21:47:23 +0300

    Задание 2 выполнено

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """
+Задание 1
 Реализуйте архитектуру приложения, используя паттерн «Фабричный
 метод».
 Представьте, что вы создали программу управления доставкой еды. В
@@ -13,7 +14,7 @@
 """
 from abc import ABC, abstractmethod
 
-
+"""
 class Transport(ABC):
 
     @abstractmethod
@@ -23,6 +24,7 @@ class Transport(ABC):
     @abstractmethod
     def get_max_weight(self):
         raise NotImplementedError
+
 
 class Scooter(Transport):
     __middle_speed = 8
@@ -37,6 +39,7 @@ class Scooter(Transport):
     def __str__(self):
         return f"Максимальный груз: {self.__max_weight} кг, средняя скорость: {self.__middle_speed}"
 
+
 class Bike(Transport):
     __middle_speed = 12
     __max_weight = 6
@@ -49,6 +52,7 @@ class Bike(Transport):
 
     def __str__(self):
         return f"Максимальный груз: {self.__max_weight} кг, средняя скорость: {self.__middle_speed}"
+
 
 class Car(Transport):
     __middle_speed = 25
@@ -76,33 +80,114 @@ class ScooterDeliver(Deliver):
     def create_transport(self):
         return Scooter()
 
+
 class BikeDeliver(Deliver):
 
     def create_transport(self):
         return Bike()
 
+
 class CarDeliver(Deliver):
 
     def create_transport(self):
         return Car()
+"""
+"""
+Задание 2.
+Реализуйте архитектуру приложения, используя паттерн «Строитель».
+Чтобы построить стандартный дом, нужно поставить 4 стены,
+установить двери, вставить пару окон и постелить крышу. Но что, если вы
+хотите дом побольше, посветлее, с бассейном, садом и прочим добром?
+Паттерн предлагает разбить процесс конструирования объекта на
+отдельные шаги (например, построить стены, вставить двери и т.д.) Чтобы
+создать объект, вам нужно поочерёдно вызывать методы строителя. Причём
+не нужно запускать все шаги, а только те, что нужны для производства
+объекта определённой конфигурации.
+"""
+
+
+class House:
+    def __init__(self):
+        self.walls = 0
+        self.doors = 0
+        self.windows = 0
+        self.swimming_pool = False
+        self.garden = False
+        self.garage = False
+
+
+class Builder(ABC):
+
+    @abstractmethod
+    def create(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_walls(self, value):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_doors(self, value):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_windows(self, value):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_swimming_pool(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_garden(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_garage(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_house(self):
+        raise NotImplementedError
+
+
+class BrickHouseBuilder(Builder):
+    __house: House
+
+    def create(self):
+        self.__house = House()
+
+    def set_walls(self, value):
+        self.__house.walls = value
+
+    def set_doors(self, value):
+        self.__house.doors = value
+
+    def set_windows(self, value):
+        self.__house.windows = value
+
+    def set_swimming_pool(self):
+        self.__house.swimming_pool = True
+
+    def set_garden(self):
+        self.__house.garden = True
+
+    def set_garage(self):
+        self.__house.garage = True
+
+    def get_house(self):
+        return self.__house
 
 
 def execute_application():
-
-    scooter_deliver = ScooterDeliver()
-    scooter = scooter_deliver.create_transport()
-    print(scooter)
-    print(scooter.get_max_weight(), scooter.get_middle_speed())
-
-    bike_deliver = BikeDeliver()
-    bike = bike_deliver.create_transport()
-    print(bike)
-    print(bike.get_max_weight(), bike.get_middle_speed())
-
-    car_deliver = CarDeliver()
-    car = car_deliver.create_transport()
-    print(car)
-    print(car.get_max_weight(), car.get_middle_speed())
+    builder = BrickHouseBuilder()
+    builder.create()
+    builder.set_walls(4)
+    builder.set_doors(2)
+    builder.set_windows(4)
+    builder.set_garage()
+    house = builder.get_house()
+    print(house.__dict__)
 
 
 if __name__ == "__main__":
